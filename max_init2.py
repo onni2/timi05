6b958048ffa1f22123dec89c0193adf8611aa25a
Onni 2 <odinnkarl@gmail.com>
2020-09-03 10:15:45 +0000

    rename again

---
+++ max_init2.py
@@ -0,0 +1,8 @@
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+while num_int > 0:
+        num_int = int(input("Input a number: "))    # Do not change this line
+        max_int += num_int
+
+    
+print("The maximum is", max_int)    # Do not change this line
