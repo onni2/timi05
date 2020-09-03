baa70286370c91affba25c71aca3be177a806fd6
Onni 2 <odinnkarl@gmail.com>
2020-09-03 10:30:25 +0000

    Algorithm written for sequence

--- max_init.py
+++ max_init.py
@@ -1,8 +1,10 @@
 num_int = int(input("Input a number: "))    # Do not change this line
 # Fill in the missing code
+max_int = num_int
 while num_int > 0:
         num_int = int(input("Input a number: "))    # Do not change this line
-        max_int += num_int
+        if num_int > max_int:
+                max_int =num_int
 
     
 print("The maximum is", max_int)    # Do not change this line
