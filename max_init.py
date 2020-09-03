1945e9dc855dcd84da83b36df065d1060d864282
Onni 2 <odinnkarl@gmail.com>
2020-09-03 11:02:55 +0000

    Algorithm written for sequence

--- max_init.py
+++ max_init.py
@@ -1,10 +1,5 @@
-num_int = int(input("Input a number: "))    # Do not change this line
-# Fill in the missing code
-max_int = num_int
-while num_int > 0:
-        num_int = int(input("Input a number: "))    # Do not change this line
-        if num_int > max_int:
-                max_int =num_int
-
-    
-print("The maximum is", max_int)    # Do not change this line
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+Sum = 1
+for i in range(1, n+1):
+        print(Sum)
+        Sum = ((n)*(2*1 + (n -1) * 1) / 2+2 * (pow(2,n) - 1)/ (2 -1))
