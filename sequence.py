1e58c78cb46576a06cd5c22ccf5d87ba8c91a49c
Onni 2 <odinnkarl@gmail.com>
2020-09-03 11:18:01 +0000

    fix

---
+++ sequence.py
@@ -0,0 +1,14 @@
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+a1 = 1
+a2 = 2
+a3 = 3
+a4 = 0
+print(a1)
+print(a2)
+print(a3)
+
+a4 = a1 + a2 + a3
+a1 = a2
+a2 = a3
+a3 = a4
+print(a4)
